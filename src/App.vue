9292aef1c5a8f33a10a4c806fb9e4fe6f9bc019f
Jack <904916790@qq.com>
2025-03-09 21:39:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app">
+        你好啊！！
+    </div>
+</template>
+
+
+
+<script>
+
+
+</script>
+
+
+<style>
+    .app{
+        background: #ddd;
+        border: 1px solid #ddd;
+    }
+</style>
